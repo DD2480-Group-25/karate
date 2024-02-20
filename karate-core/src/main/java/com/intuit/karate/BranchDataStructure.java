7cdfac408bbb5df213ce3922e3caa129f771eb81
Anneli Bogren <annbog@kth.se>
2024-02-20 11:38:31 +0100

    feat: write tests for Config (#3)
    Co-authored-by: goodtimeswithpaul <moi@paultissot.ch>

--- karate-core/src/main/java/com/intuit/karate/BranchDataStructure.java
+++ karate-core/src/main/java/com/intuit/karate/BranchDataStructure.java
@@ -55,7 +55,7 @@ public class BranchDataStructure {
                 count++;
             }
         }
-        double result = count / branchCount;
+        double result = count / branchCount * 100;
         return result;
     }
 
