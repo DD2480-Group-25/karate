2472be9a3c895cef2b432544ac216dc18e7ac400
WarlCang <carlwang1124@outlook.com>
2024-02-21 08:20:16 +0100

    feat: testCorsPreflight(#12)
    Tests branch 1, one public method added to modify value in mockhandler.

--- karate-core/src/main/java/com/intuit/karate/core/MockHandler.java
+++ karate-core/src/main/java/com/intuit/karate/core/MockHandler.java
@@ -78,6 +78,9 @@ public class MockHandler implements ServerHandler {
     protected static final ThreadLocal<Request> LOCAL_REQUEST = new ThreadLocal<>();
     private final String prefix;
 
+    public void setCors(boolean corsEnabled){
+        this.corsEnabled = corsEnabled;
+    }
     public MockHandler(Feature feature) {
         this(feature, null);
     }
